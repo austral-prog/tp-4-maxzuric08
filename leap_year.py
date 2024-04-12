8dceacfd0995f42c42c08d6f8e20bd438e2ce808
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-04-12 10:29:19 -0400

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    a=input()
+    print('Ingrese un año:',a)
+    if a%4==0 and a%1000==0:
+        if a%400:
+            print('El año',a,'es bisiesto')
+        else:
+            print('El año',a,'no es bisiesto')
+    elif a%4==0:
+        print('El año',a,'es bisiesto')
+    else:
+        print('El año',a,'no es bisiesto')
